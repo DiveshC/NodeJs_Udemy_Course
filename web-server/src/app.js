64eff99c36cabe0de4be36887dc784b9e564a65f
DiveshC <drchudasama26@gmail.com>
2019-05-15 11:44:11 -0400

    Section 7

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,39 @@
+const path=require('path');
+
 const express = require('express');
 
-const app = express();
 
-app.get('',(req,res) =>{
-    res.send('Hello express!');
+const app = express();
+const addr = path.join(__dirname, '../public');
+
+app.set('view engine', 'hbs');
+app.use(express.static(addr));
+
+app.get('', (req,res) => {
+    res.render('index', {
+        title:'Weather App',
+        name:'Divesh C.'
+    });
+});
+
+app.get('/about',(req, res)=>{
+    res.render('about', {
+        title:'Weather App',
+        name:'Divesh C.'
+    });
 });
 
 app.get('/help',(req, res)=>{
-    res.send('Help page');
-})
-
-app.get('/about',(req,res) =>{
-    res.send('About Page');
+    res.render('help',{
+        helpText: "Text",
+    });
 });
 
 app.get('/weather',(req, res)=>{
-    res.send('Weather page');
+    res.send({
+        forecast:'24 degrees',
+        location:'Toronto'
+    });
 });
 
 app.listen(3000, () =>{
