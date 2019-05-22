6fde9cac4217bfbb2b57725e72bde7176f207882
DiveshC <drchudasama26@gmail.com>
2019-05-22 13:36:44 -0400

    Section 7 completed

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,23 @@
 const path=require('path');
-
+const hbs = require('hbs');
 const express = require('express');
 
 
 const app = express();
-const addr = path.join(__dirname, '../public');
 
+//DEFINE PATHS 
+const addr = path.join(__dirname, '../public');
+const viewPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+//setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewPath);
+hbs.registerPartials(partialsPath);
+
+//setup static directory to serve
 app.use(express.static(addr));
+
 
 app.get('', (req,res) => {
     res.render('index', {
@@ -26,6 +36,8 @@ app.get('/about',(req, res)=>{
 app.get('/help',(req, res)=>{
     res.render('help',{
         helpText: "Text",
+        title:'Help',
+        name:'Divesh C.'
     });
 });
 
@@ -33,6 +45,23 @@ app.get('/weather',(req, res)=>{
     res.send({
         forecast:'24 degrees',
         location:'Toronto'
+    });
+});
+
+app.get('/help/*', (req, res) =>{
+    res.render('404page', {
+        title:'404',
+        error: 'Help article not found',
+        name:'Divesh C.'
+    });
+})
+
+
+app.get('*', (req,res)=>{
+    res.render('404page', {
+        title:'404',
+        error: 'Page not found',
+        name: 'Divesh C'
     });
 });
 
