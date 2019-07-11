f4da66ab74516ec51c1136298261b5f64fe33496
DiveshC <drchudasama26@gmail.com>
2019-07-11 14:14:56 -0400

    section 13

--- playground/promises.js
+++ playground/promises.js
@@ -18,3 +18,36 @@ doWorkPromise.then((result) => {
 //                              \
 //                              Rejected
 //
+
+const add = (a,b) => {
+    return new Promise((resolve,reject) => {
+        setTimeout(()=>{
+            resolve(a+b);
+        },2000);
+    });
+}
+//Nested promises
+// add(1,2).then((sum) =>{
+//     console.log(sum);
+
+//     add(sum, 5)/then((sum2)=>{
+//         console.log(sum2);
+//     }).catch((err)=>{
+//         console.log(err);
+//     })
+// }).catch((err)=>{
+//     console.log(err);
+// })
+
+//promise chaining :
+add(1,1).then((sum)=>{
+    console.log(sum);
+
+    return add(sum, 4);
+}).then((sum2)=>{
+    console.log(sum2);
+}).catch((err)=>{
+    console.log(err);
+})
+
+
